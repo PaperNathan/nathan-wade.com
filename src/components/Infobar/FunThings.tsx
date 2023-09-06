9d750d8b00ce7b7e0301893841e06f218f058094
Nathan Wade <PaperNathan@gmail.com>
2023-09-06 09:02:32 -0500

    removed unused files and cleanup some comments (#18)

---
+++ src/components/Infobar/FunThings.tsx
@@ -0,0 +1,25 @@
+/**
+ * A String array of fun things to do.
+ */
+export default [
+  "Writing a letter",
+  "Creating a compost pile",
+  "at a local thrift shop",
+  "Learning the periodic table",
+  "Drawing",
+  "Conquering my fears",
+  "Learning how to beatbox",
+  "Writing a poem",
+  "Learning how to juggle",
+  "Writing in my journal",
+  "Making homemade ice cream",
+  "Washing my car",
+  "Writing a short story",
+  "Making a couch fort",
+  "Learning how to play chess",
+  "Learning to write with my nondominant hand",
+  "Doing Yoga",
+  "Painting"
+];
+
+
