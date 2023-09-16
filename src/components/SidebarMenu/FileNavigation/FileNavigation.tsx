6a219615bd2b6e215277d84b8457ecec3b7b5265
Nathan Wade <PaperNathan@gmail.com>
2023-09-16 01:01:02 -0500

    styling for file system nav (#21)

--- src/components/SidebarMenu/FileNavigation/FileNavigation.tsx
+++ src/components/SidebarMenu/FileNavigation/FileNavigation.tsx
@@ -1,22 +1,48 @@
 import "./FileNavigation.scss";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import { 
   InsertDriveFileOutlined, 
-  PersonFilled 
+  PersonFilled,
+  KeyboardArrowRightRound,
+  KeyboardArrowDownRound
 } from "@ricons/material";
-import SidebarMenuLink from "@/components/SidebarMenu/SidebarMenuLink/SidebarMenuLink";
 
 export default function FileNavigation() {
+  const [toggleMenu, setToggleMenu] = useState("open");
+
+  const toggleOpen = () => {
+    if (toggleMenu === "open") {
+      setToggleMenu("closed");
+    } else {
+      setToggleMenu("open");
+    }
+  }
+
+  const getArrowPosition = () => {
+    if (toggleMenu === "open") {
+      return <KeyboardArrowDownRound className="FileNavigation__arrow" />
+    } else {
+      return <KeyboardArrowRightRound className="FileNavigation__arrow" />
+    }
+  }
+
   return (
     <div className="FileNavigation">
-      <SidebarMenuLink>
-        <PersonFilled className="SidebarMenuLink__icon" />
-        <Link to="/" className="SidebarMenuLink__link">nathan.yml</Link>
-      </SidebarMenuLink>
-      <SidebarMenuLink>
-        <InsertDriveFileOutlined className="SidebarMenuLink__icon" />
-        <Link to="/about" className="SidebarMenuLink__link">README.md</Link>
-      </SidebarMenuLink>
+      <div className="FileNavigation__title" onClick={ toggleOpen }>
+        { getArrowPosition() }
+        NATHAN.WADE [CV]
+      </div>
+      <div className={`FileNavigation__files--${ toggleMenu }`}>
+        <div className="FileNavigation__fileLink">
+          <PersonFilled className="FileNavigation__fileLink--icon" />
+          <Link to="/" className="FileNavigation__fileLink--text">nathan.yml</Link>
+        </div>
+        <div className="FileNavigation__fileLink">
+          <InsertDriveFileOutlined className="FileNavigation__fileLink--icon" />
+          <Link to="/about" className="FileNavigation__fileLink--text">README.md</Link>
+        </div>
+      </div>
     </div>
   )
 }
