fd55d3b256e200b455a5ca7b3e47d851b6e06143
Nathan Wade <PaperNathan@gmail.com>
2023-12-19 23:05:26 -0600

    big updates (#57)

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,5 +1,5 @@
 import './Home.scss';
-import { Github, Linkedin, Spotify, Codepen } from "@ricons/fa";
+import { Github, Linkedin, Spotify, Codepen, Envelope } from "@ricons/fa";
 
 import Icon from "@/components/Icon/Icon";
 import Image from "@/components/Image/Image";
@@ -12,7 +12,7 @@ export default function Home() {
       <Image alt="Random Image" height={200} width={200} style={imgStyle} />
       <h1 className="Home__display">Nathan Wade</h1>
       <p className="Home__subtext">
-        Front-end Software Engineer
+        Senior Front-End Software Engineer
       </p>
       <div className="Home__info">
         <p>Currently Located in the United States</p>
@@ -22,6 +22,7 @@ export default function Home() {
         <Icon url="https://codepen.io/PaperNathan" icon={<Codepen />} />
         <Icon url="https://www.linkedin.com/in/nathanwade-/" icon={<Linkedin />} />
         <Icon url="https://open.spotify.com/user/1295192359" icon={<Spotify />} />
+        <Icon url="mailto:nathan.tyler.wade@gmail.com" icon={<Envelope />} />
       </div>
     </div>
   )
